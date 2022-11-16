17e68e8236e5f9efd985204188248378b8db8488
Park Min Seo <karaminseo@naver.com>
2022-11-17 03:39:12 +0900

    이동키 변경

--- Scene.cpp
+++ Scene.cpp
@@ -328,7 +328,7 @@ bool CScene::OnProcessingMouseMessage(HWND hWnd, UINT nMessageID, WPARAM wParam,
 
 bool CScene::OnProcessingKeyboardMessage(HWND hWnd, UINT nMessageID, WPARAM wParam, LPARAM lParam)
 {
-	switch (nMessageID)
+	/*switch (nMessageID)
 	{
 	case WM_KEYDOWN:
 		switch (wParam)
@@ -345,7 +345,7 @@ bool CScene::OnProcessingKeyboardMessage(HWND hWnd, UINT nMessageID, WPARAM wPar
 		break;
 	default:
 		break;
-	}
+	}*/
 	return(false);
 }
 
