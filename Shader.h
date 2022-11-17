573fc8afd7ca8d62fe47ee1a84454da7b62b4932
Park Min Seo <karaminseo@naver.com>
2022-11-17 23:01:36 +0900

    CTerrainWaterShader 클래스 추가

--- Shader.h
+++ Shader.h
@@ -164,6 +164,22 @@ public:
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //
+class CTerrainWaterShader : public CStandardShader
+{
+public:
+	CTerrainWaterShader();
+	virtual ~CTerrainWaterShader();
+
+	virtual D3D12_INPUT_LAYOUT_DESC CreateInputLayout();
+
+	virtual D3D12_SHADER_BYTECODE CreateVertexShader(ID3DBlob** ppd3dShaderBlob);
+	virtual D3D12_SHADER_BYTECODE CreatePixelShader(ID3DBlob** ppd3dShaderBlob);
+
+	virtual void CreateShader(ID3D12Device* pd3dDevice, ID3D12GraphicsCommandList* pd3dCommandList, ID3D12RootSignature* pd3dGraphicsRootSignature);
+};
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
 class CPlayerShader : public CShader
 {
 public:
